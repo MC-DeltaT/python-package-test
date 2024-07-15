03046a6c2731703c96bbfca94ce2a92a9b4b4e94
Reece Jones <reece.jones131@gmail.com>
2024-07-15 21:05:30 +1000

    GitHub Action for dev workflow testing

--- dev-scripts/clean.sh
+++ dev-scripts/clean.sh
@@ -3,4 +3,5 @@
 set -e
 
 echo "Deleting all dev and build artifacts"
-rm -rf .venv .tox .ruff_cache *.egg-info dist
+rm -rf .venv .tox .ruff_cache *.egg-info dist .mypy_cache .pytest_cache
+find . -name __pycache__ -type d -exec rm -rf {} +
